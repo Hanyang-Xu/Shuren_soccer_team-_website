bd2b4786f00848d57fb74860870a4a564b12ce77
Hanyang Xu <xuhanyang593@gmail.com>
2026-05-13 14:46:09 +0800

    Update images.js

--- images.js
+++ images.js
@@ -11,34 +11,36 @@ const siteImages = {
   },
 
   // 球队相册图片
-  // image 可以写本地图片路径，例如：images/training01.jpg
-  // caption 是图片下方显示的文字
-  // alt 是图片加载失败时的替代文字
   gallery: [
     {
-      image: "images/team.jpg",
-      caption: "球队大合照",
-      alt: "树仁书院足球队合照"
+      image: "images/group_photo_2023.jpg",
+      caption: "2023 年球队合照",
+      alt: "2023 年树仁书院足球队合照"
     },
     {
-      image: "images/training01.jpg",
-      caption: "球队训练瞬间",
-      alt: "球队训练"
+      image: "images/group_photo_2023_2.jpg",
+      caption: "2023 年球队活动",
+      alt: "2023 年球队活动照片"
     },
     {
-      image: "images/match01.jpg",
-      caption: "比赛现场",
-      alt: "比赛现场"
+      image: "images/group_photo_2024.jpg",
+      caption: "2024 年球队合照",
+      alt: "2024 年树仁书院足球队合照"
     },
     {
-      image: "images/field01.jpg",
-      caption: "绿茵场",
-      alt: "足球场"
+      image: "images/group_photo_2025.jpg",
+      caption: "2025 年球队合照",
+      alt: "2025 年树仁书院足球队合照"
     },
     {
-      image: "images/team-spirit.jpg",
-      caption: "团队精神",
-      alt: "团队合照"
+      image: "images/group_photo_2025_1.jpg",
+      caption: "2025 年球队瞬间",
+      alt: "2025 年球队照片"
+    },
+    {
+      image: "images/group_photo_2025_2.jpg",
+      caption: "2025 年球队活动",
+      alt: "2025 年球队活动照片"
     }
   ]
 };
