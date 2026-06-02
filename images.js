e47beb0c1fa60ed00dd548e69370e32079a307e5
Hanyang Xu <xuhanyang593@gmail.com>
2026-06-02 14:36:14 +0800

    Update images.js

--- images.js
+++ images.js
@@ -9,13 +9,13 @@ const siteImages = {
     caption: "2026 年南方科技大学第十届“树说杯”足球赛合照"
   },
   gallery: [
-    { image: "images/group_photo_2026.jpg", caption: "2026 年树说杯球队合照", alt: "2026 年树仁书院足球队树说杯合照" },
+    { image: "images/group_photo_2026.jpg", caption: "2026 年书院杯球队合照", alt: "2026 年树仁书院足球队书院杯合照" },
     { image: "images/group_photo_2023.jpg", caption: "2023 年球队合照", alt: "2023 年树仁书院足球队合照" },
-    { image: "images/group_photo_2023_2.jpg", caption: "2023 年球队活动", alt: "2023 年球队活动照片" },
+    { image: "images/group_photo_2023_2.jpg", caption: "2023 年球队照片", alt: "2023 年球队照片" },
     { image: "images/group_photo_2024.jpg", caption: "2024 年球队合照", alt: "2024 年树仁书院足球队合照" },
     { image: "images/group_photo_2025.jpg", caption: "2025 年球队合照", alt: "2025 年树仁书院足球队合照" },
     { image: "images/group_photo_2025_1.jpg", caption: "2025 年球队瞬间", alt: "2025 年球队照片" },
-    { image: "images/group_photo_2025_2.jpg", caption: "2025 年球队活动", alt: "2025 年球队活动照片" }
+    { image: "images/group_photo_2025_2.jpg", caption: "2025 年球队照片", alt: "2025 年球队照片" }
   ]
 };
 
